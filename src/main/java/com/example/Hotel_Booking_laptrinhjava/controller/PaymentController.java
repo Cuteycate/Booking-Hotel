a9329ed29da905f0b1ba7d51747dcaf1cb6c447c
KaeseyVNK <128293584+KaeseyVNK@users.noreply.github.com>
2024-06-22 23:05:40 +0700

    + Chức năng gửi Email
    nhớ reload lại Maven

--- src/main/java/com/example/Hotel_Booking_laptrinhjava/controller/PaymentController.java
+++ src/main/java/com/example/Hotel_Booking_laptrinhjava/controller/PaymentController.java
@@ -1,6 +1,7 @@
 package com.example.Hotel_Booking_laptrinhjava.controller;
 
 import com.example.Hotel_Booking_laptrinhjava.response.PaymentResponse;
+import com.example.Hotel_Booking_laptrinhjava.service.EmailService;
 import com.example.Hotel_Booking_laptrinhjava.service.VNPayService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
@@ -16,6 +17,10 @@ public class PaymentController {
 
     @Autowired
     private VNPayService vnPayService;
+
+    @Autowired
+    private EmailService emailService;
+
 
     @PostMapping("/submitOrder")
     public ResponseEntity<String> submitOrder(@RequestParam("amount") int roomPrice,
@@ -40,4 +45,34 @@ public class PaymentController {
 
         return ResponseEntity.ok(payment);
     }
+
+    @PostMapping("/emailSender")
+    public ResponseEntity<String> emailSender(@RequestParam("confirmationCode") String confirmationCode,
+                                              @RequestParam("guestEmail") String guestEmail) {
+        // Tiêu đề và nội dung email
+        String subject = "Your Booking Confirmation - Thank You for Choosing Us!";
+        String body = "Dear Valued Guest,\n\n" +
+                "We are thrilled to confirm your booking with us! It is our pleasure to have you as our guest and we are committed to making your stay memorable.\n\n" +
+                "Here are your booking details:\n" +
+                "Confirmation Code: " + confirmationCode + "\n\n" +
+                "Please keep this confirmation code handy for your reference.\n\n" +
+                "If you have any questions or need further assistance, do not hesitate to contact us. We are here to help you and ensure you have a wonderful experience.\n\n" +
+                "Thank you for choosing our hotel. We look forward to welcoming you soon!\n\n" +
+                "Warm regards,\n" +
+                "The Hotel Booking Team\n" +
+                "Contact Us: [Penacony Hotel Team ]\n" +
+                "Website: [Penacony Hotel]\n";
+
+        // Gửi email
+        emailService.sendEmail(guestEmail, subject, body);
+
+        // Trả về phản hồi cho client
+        return ResponseEntity.ok("Confirmation email sent to " + guestEmail);
+    }
+
+
+
+
+
+
 }
